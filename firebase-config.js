c408efe6c77f80b40fe35acb7d5d37f962f71a27
wuttichaikoji-oss <wuttichaikoji@gmail.com>
2026-03-11 23:20:48 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,10 @@
+window.LAYA_FIRESTORE_DATABASE_ID = "laya";
 window.LAYA_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyB7t2WW7zmZnXVWXujKE0ayYjB-dB3MlpI",
+  authDomain: "laya-order.firebaseapp.com",
+  projectId: "laya-order",
+  storageBucket: "laya-order.firebasestorage.app",
+  messagingSenderId: "969828212139",
+  appId: "1:969828212139:web:d09782b21570f1cfa106ee",
+  measurementId: "G-KXEFLQF3HN"
 };
